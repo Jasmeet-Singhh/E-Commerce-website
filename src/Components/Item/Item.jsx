e79b1dbccd748038337d0ca99a346ff67bf1617b
Jasmeet Singh <jasmeetsinghworking@gmail.com>
2024-03-02 21:15:49 +0530

    Complete shop-p-section

---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,19 @@
+import style from "./Item.module.css";
+const Item = (props) => {
+    return (
+        <div className={style.item}>
+            <img src={props.image} alt="img" />
+            <p>{props.name}</p>
+            <div className={style["item-prices"]}>
+                <div className={style["item-price-new"]}>
+                    ${props.new_price}
+                </div>
+                <div className={style["item-price-old"]}>
+                    ${props.old_price}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
