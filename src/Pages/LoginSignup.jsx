1a9d6dfdae377e6c7fc0d2349451ac1660488b0c
Jasmeet Singh <jasmeetsinghworking@gmail.com>
2024-03-06 12:03:33 +0530

    Complete login-signup-section

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import style from "./CSS/LoginSignup.module.css";
 
-const LoginSignup = ()=>{
-    return(
-        <div>
-            
+const LoginSignup = () => {
+    return (
+        <div className={style["loginSignup"]}>
+            <div className={style["loginSignup-container"]}>
+                <h1>Sigh Up</h1>
+                <div className={style["loginSignup-fields"]}>
+                    <input type="text" placeholder="Your Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>Continue</button>
+                <p className={style["loginSignup-login"]}>Already have an account? <span>Login here</span></p>
+                <div className={style["loginSignup-agree"]}>
+                   <input type="checkbox" name="" id="" />
+                   <p>By continuing, i agree to the terms of use and privacy policy.</p>
+                </div>
+            </div>
         </div>
     )
 }
